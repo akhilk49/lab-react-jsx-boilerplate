fd2d6de7530d21c449565ed1a2b25a0f75af763a
akhilk49 <akhilkkurian49@gmail.com>
2024-01-08 16:50:43 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,20 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  const data = imageData(); 
+ 
+  return ( 
+    <div>
+      <div className='title'>
+        <h1>Kalvium Gallery</h1>
+      </div>
+      <div className='grid'>
+      {data.map((ele) => ( 
+        <img id={ele.id} src={ele.img} alt={`${ele.id}`} /> 
+      ))} 
+      </div>
+    </div> 
+  ); 
 }
 
 export default App;
